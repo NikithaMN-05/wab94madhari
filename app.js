e84c1b25f981d9b67dd4033d0e384ca12b901ec1
NikithaMN-05 <77766697+NikithaMN-05@users.noreply.github.com>
2021-03-31 21:39:05 -0500

    added stars

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var icecreamRouter = require('./routes/icecream');
-var starRouter = require('./routes/stars');
-
 
 var app = express();
 
@@ -24,9 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/icecream', icecreamRouter);
-app.use('/stars', starsRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
